960a78d19df63586cb7d9c7cab6ef5edeeba8eb4
eyan ingles <eyaningles29@gmail.com>
2026-05-10 12:14:45 +1000

    Initial Commit

---
+++ personal-ticketing-app/personal_ticketing_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  personal_ticketing_appApp.swift
+//  personal-ticketing-app
+//
+//  Created by Eyan Ingles on 10/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct personal_ticketing_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
