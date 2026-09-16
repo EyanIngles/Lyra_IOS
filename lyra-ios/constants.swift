3edeb8973053a3432b1fe5185cece20681bfa617
eyan ingles <eyaningles29@gmail.com>
2026-09-16 21:46:46 +1000

    Chore: lyra-ios housekeeping and Lyrindra Labs identity
    Rename the Xcode project and sources to lyra-ios, add gitignore
    (xcuserdata, DerivedData, local work tracker), fix Info.plist, and
    set organization/copyright to Lyrindra Labs.

---
+++ lyra-ios/constants.swift
@@ -0,0 +1,11 @@
+//
+//  constants.swift
+//  lyra-ios
+//
+//  Created by Lyrindra Labs on 13/6/2026.
+//
+struct Constants {
+    static var baseURL_macbook = "https://macbook-pro.tailcb4684.ts.net:3000" //Device: new macbook pro M5 Max.
+    static var baseURL_pi = "https://pi.tailcb4684.ts.net:3000" //Device: Pi Server that is the final product to run the server.
+    
+}
